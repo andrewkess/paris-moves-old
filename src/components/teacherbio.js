9c6748d5bcfe25ec4b653259e745470a0c6bde42
Andrew Kessinger <53582334+andrewkess@users.noreply.github.com>
2020-04-08 01:03:49 +0200

    Update teacherbio.js

--- src/components/teacherbio.js
+++ src/components/teacherbio.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './teacher.module.css'
-import Img from 'gatsby-image'
 import instagram from "../../public/instagram.png" // Tell Webpack this JS file uses this image
 import { Image, Container, Row, Col } from 'react-bootstrap';
 
@@ -10,7 +9,7 @@ export default ({ data2 }) => (
 
     <div className={styles.teacherDetails}>
       <div className={styles.teacherName}>{data2.name}
-     
+      <Image src={instagram} className={styles.insta} />
 </div>
 
 
@@ -18,7 +17,7 @@ export default ({ data2 }) => (
 <Row>
 <Col xs={11} sm={11} md={11} lg={11}>{data2.name}
 </Col>
-<Col xs={1} sm={1} md={1} lg={1}> 
+<Col xs={1} sm={1} md={1} lg={1}> <Image src={instagram} fluid />
 </Col>
 </Row>
 </Container>
